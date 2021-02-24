4cd14c8c7bef2784dfdaa90bc5ecca16a4d0d18a
Lucas Russo <lerwys@gmail.com>
2021-02-24 12:56:55 -0300

    sim/rtm_model/Manifest.py: improve readability with one file per line

--- hdl/sim/rtm_model/Manifest.py
+++ hdl/sim/rtm_model/Manifest.py
@@ -1,1 +1,8 @@
-files = ["magnet_model.vhd", "ffd.vhd","dac8831_model.vhd", "ddr_des.vhd", "ltc232x_model.vhd", "rtm_lamp_model.vhd"]
+files = [
+    "magnet_model.vhd",
+    "ffd.vhd",
+    "dac8831_model.vhd",
+    "ddr_des.vhd",
+    "ltc232x_model.vhd",
+    "rtm_lamp_model.vhd",
+]
