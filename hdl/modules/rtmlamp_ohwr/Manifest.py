569437832c0289798b171f288624ce8658f31697
Lucas Russo <lerwys@gmail.com>
2021-03-01 22:25:18 -0300

    rtmlamp_ohwr: add missing modules to Manifest

--- hdl/modules/rtmlamp_ohwr/Manifest.py
+++ hdl/modules/rtmlamp_ohwr/Manifest.py
@@ -2,4 +2,8 @@ files = [
     "ltc232x_acq.vhd",
     "multi_dac_spi.vhd",
     "rtmlamp_ohwr_serial_regs.vhd",
+    "rtmlamp_ohwr.vhd",
+    "wb_rtmlamp_ohwr.vhd",
+    "xwb_rtmlamp_ohwr.vhd",
+    "cheby/wb_rtmlamp_ohwr_regs.vhd",
 ]
