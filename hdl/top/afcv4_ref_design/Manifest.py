bea7bea9b26b2f03e4198a9dd3919135217afdbe
Lucas Russo <lerwys@gmail.com>
2021-04-23 08:32:01 -0300

    {syn, top}: add initial port to AFCv4

---
+++ hdl/top/afcv4_ref_design/Manifest.py
@@ -0,0 +1,16 @@
+files = [
+    "afcv4_rtm_lamp_ctrl.vhd",
+]
+
+fetchto = "../../ip_cores"
+
+modules = {
+    "local" : [
+        "../..",
+    ],
+    "git" : [
+        "https://github.com/lnls-dig/infra-cores.git",
+        "https://github.com/lnls-dig/general-cores.git",
+        "https://github.com/lnls-dig/afc-gw.git",
+    ],
+}
