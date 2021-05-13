e04578c019628cd802c0e92bd7e3f100a7eb34d1
Lucas Russo <lerwys@gmail.com>
2021-05-13 14:43:00 -0300

    pi_controller: add valid logic to ctrl_fb signal

--- hdl/modules/rtmlamp_ohwr/pi_controller.vhd
+++ hdl/modules/rtmlamp_ohwr/pi_controller.vhd
@@ -45,25 +45,39 @@ entity pi_controller is
       ctrl_sp_i: in  std_logic_vector(g_PRECISION-1 downto 0);
       -- Controller feedback signal (2's complement)
       ctrl_fb_i:  in  std_logic_vector(g_PRECISION-1 downto 0);
+      -- Controller feedback valid signal
+      ctrl_fb_valid_i: in std_logic;
       -- Controller output signal (2's complement)
-      ctrl_sig_o: out std_logic_vector(g_PRECISION-1 downto 0)
+      ctrl_sig_o: out std_logic_vector(g_PRECISION-1 downto 0);
+      -- Controller output valid signal
+      ctrl_sig_valid_o: out std_logic
     );
 end pi_controller;
 
 architecture pi_controller_arch of pi_controller is
   signal pre_acc: signed((g_PRECISION*2) downto 0) := (others => '0');
+  signal pre_acc_valid : std_logic;
   signal acc: signed((g_PRECISION*2)-1 downto 0) := (others => '0');
+  signal acc_valid : std_logic;
   signal sum: signed(g_PRECISION downto 0) := (others => '0');
+  signal sum_valid : std_logic;
   signal err: signed(g_PRECISION-1 downto 0);
+  signal err_valid : std_logic;
   signal kp_reg: std_logic_vector(g_PRECISION-1 downto 0);
   signal ti_reg: std_logic_vector(g_PRECISION-1 downto 0);
   signal err_kp_pre: signed((g_PRECISION*2)-1 downto 0);
   signal err_ti_pre: signed((g_PRECISION*2)-1 downto 0);
+  signal err_mult_pre_valid : std_logic;
   signal err_kp: signed((g_PRECISION*2)-1 downto 0);
   signal err_ti: signed((g_PRECISION*2)-1 downto 0);
+  signal err_mult_valid : std_logic;
   signal err_kp_shifted: signed((g_PRECISION*2)-1 downto 0) := (others => '0');
   signal err_ti_shifted: signed((g_PRECISION*2)-1 downto 0) := (others => '0');
+  signal err_mult_shifted_valid : std_logic;
+  signal ctrl_fb_valid: std_logic;
+  signal ctrl_fb: std_logic_vector(g_PRECISION-1 downto 0);
   signal ctrl_sig: std_logic_vector(g_PRECISION-1 downto 0);
+  signal ctrl_sig_valid : std_logic;
 
   constant c_ctrl_sig_max: std_logic_vector(ctrl_sig'range) := '0' & (ctrl_sig'left-1 downto 0 => '1');
   constant c_ctrl_sig_min: std_logic_vector(ctrl_sig'range) := '1' & (ctrl_sig'left-1 downto 0 => '0');
@@ -138,26 +152,40 @@ begin
 
   err_ti_shifted <= shift_right(err_ti, ti_shift_i);
   err_kp_shifted <= shift_right(err_kp, kp_shift_i);
+  err_mult_shifted_valid <= err_mult_valid;
 
   process(clk_i)
-    constant c_acc_max: signed(acc'range) := '0' & (acc'left-1 downto 0 => '1');
-    constant c_acc_min: signed(acc'range) := '1' & (acc'left-1 downto 0 => '0');
   begin
     if rising_edge(clk_i) then
       if rst_n_i = '0' then
         pre_acc <= (others => '0');
+        pre_acc_valid <= '0';
         acc <= (others => '0');
+        acc_valid <= '0';
         sum <= (others => '0');
+        sum_valid <= '0';
         err <= (others => '0');
+        err_valid <= '0';
         ti_reg <= (others => '0');
         kp_reg <= (others => '0');
         err_ti_pre <= (others => '0');
         err_kp_pre <= (others => '0');
+        err_mult_pre_valid <= '0';
         err_ti <= (others => '0');
         err_kp <= (others => '0');
+        err_mult_valid <= '0';
         ctrl_sig <= (others => '0');
+        ctrl_sig_valid <= '0';
+        ctrl_fb <= (others => '0');
+        ctrl_fb_valid <= '0';
       else
-        err <= signed(ctrl_sp_i) - signed(ctrl_fb_i);
+        ctrl_fb_valid <= ctrl_fb_valid_i;
+        if ctrl_fb_valid_i = '1' then
+          ctrl_fb <= ctrl_fb_i;
+        end if;
+
+        err <= signed(ctrl_sp_i) - signed(ctrl_fb);
+        err_valid <= ctrl_fb_valid;
 
         -- input mult pipeline reg
         ti_reg <= ti_i;
@@ -165,27 +193,38 @@ begin
         -- mult
         err_ti_pre <= signed(ti_reg) * err;
         err_kp_pre <= signed(kp_reg) * err;
+        err_mult_pre_valid <= err_valid;
         -- output mult pipeline reg
         err_ti <= err_ti_pre;
         err_kp <= err_kp_pre;
+        err_mult_valid <= err_mult_pre_valid;
 
         -- integral stage
-        if not ((signed(sum) >= signed(c_ctrl_sig_max) and signed(err_ti_shifted) > 0) or
-                (signed(sum) <= signed(c_ctrl_sig_min) and signed(err_ti_shifted) < 0)) then
+        if err_mult_shifted_valid = '1' and (
+                not ((signed(sum) >= signed(c_ctrl_sig_max) and signed(err_ti_shifted) > 0) or
+                (signed(sum) <= signed(c_ctrl_sig_min) and signed(err_ti_shifted) < 0))) then
           pre_acc <= resize(signed(acc), pre_acc'length) +
                      resize(err_ti_shifted, pre_acc'length);
         end if;
+        pre_acc_valid <= err_mult_shifted_valid;
 
         acc <= signed(f_saturate(std_logic_vector(pre_acc), acc'left));
+        acc_valid <= pre_acc_valid;
 
         -- proportional stage
-        sum <= resize(acc(acc'left downto g_PRECISION), sum'length) +
-                resize(err_kp_shifted(err_kp_shifted'left downto g_PRECISION), sum'length);
+        if acc_valid = '1' then
+          sum <= resize(acc(acc'left downto g_PRECISION), sum'length) +
+                  resize(err_kp_shifted(err_kp_shifted'left downto g_PRECISION), sum'length);
+        end if;
+        sum_valid <= acc_valid;
+
         ctrl_sig <= f_saturate(std_logic_vector(sum), ctrl_sig'left);
+        ctrl_sig_valid <= sum_valid;
 
       end if;
     end if;
   end process;
 
   ctrl_sig_o <= ctrl_sig;
+  ctrl_sig_valid_o <= ctrl_sig_valid;
 end pi_controller_arch;
